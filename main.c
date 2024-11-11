8597813e7402a2cad900f513e1c8cea3c48d285e
RenderHoward <David.Howard@Ultrapoise.com>
2024-11-11 13:32:35 -0500

    Added standard spherical trigonometric formulas for bearing and final location. Gives result assuming radar traveled along surface of Earth.

--- main.c
+++ main.c
@@ -12,13 +12,15 @@ const double g_EarthRadius = 6371000;
 
 const int SUCCESS = 0;
 
-double pi = 3.1416926;
+const int ERROR = -1;
+
+const double pi = 3.1415926;
 
 static inline double radian( double theta ) { return pi * theta / 180; }
 
 static inline double degree( double theta ) { return 180 * theta / pi; }
 
-double haversine( double theta) {  return (1 - cos(theta)) / 2; }
+static inline double haversine( double theta) {  return (1 - cos(theta)) / 2; }
 
 int GIStoRadar(double *range,
                double *bearing,
@@ -27,12 +29,25 @@ int GIStoRadar(double *range,
                double glonFinal,
                double glatFinal )
 {
+    if( bearing == NULL || range == NULL )
+        return ERROR;
+
     double delta_phi = radian( glatFinal - glatInit ), delta_lambda = radian( glonFinal - glonInit ),
             phi_init = radian( glatInit ), phi_final = radian( glatFinal );
 
     double haversine_theta = haversine(delta_phi) + cos(phi_init)*cos(phi_final)*haversine(delta_lambda);
 
     *range = 2 * g_EarthRadius * asin( sqrt( haversine_theta ) );
+
+    *bearing =
+        degree
+        (
+            atan2
+            (
+                sin(delta_lambda)*cos(phi_final),
+                cos(phi_init)*sin(phi_final) - sin(phi_init)*cos(phi_final)*cos(delta_lambda)
+            )
+        );
 
     return SUCCESS;
 }
@@ -44,5 +59,27 @@ int RadartoGis(double range,
                double *glonFinal,
                double *glatFinal)
 {
+    if( glonFinal == NULL || glatFinal == NULL )
+        return ERROR;
+
+    double delta = range / g_EarthRadius;   // Central angle along great circle
+
+    double phi_init = radian(glatInit),
+           phi_final = asin( sin(phi_init) * cos(delta) + cos(phi_init) * sin(delta) * cos(delta));
+
+    *glatFinal = degree( phi_final );
+
+    double lambda_init = radian(glonInit);
+
+    *glonFinal = degree
+                 (
+                    lambda_init +
+                    atan2
+                    (
+                        sin(radian(bearing)) * sin(delta) * cos(phi_init),
+                        cos(delta) - sin(phi_init)*sin(phi_final)
+                    )
+                );
+
     return SUCCESS;
 }
